a44ff29ea8bfc23c6d91c5d9fe03a0f78667c260
Shu Jin <mrking@foxmail.com>
2023-02-09 20:18:49 -0500

    add commits

--- COMP229_Lab1/routes/index.js
+++ COMP229_Lab1/routes/index.js
@@ -9,24 +9,32 @@ Date: 2023-02-06
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET default first page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home' , page: "home"});
 });
 
+// GET home page
 router.get("/home", function (req, res, next) {
   res.render("index", { title: "Home", page: "home" });
 });
 
+// GET about page
 router.get('/about', function(req, res, next) {
   res.render('index', { title: 'About', page: "about" });
 });
+
+// GET projects page
 router.get('/projects', function(req, res, next) {
   res.render('index', { title: 'Projects', page: "projects" });
 });
+
+// GET contact page
 router.get('/contact', function(req, res, next) {
   res.render('index', { title: 'Contact', page: "contact" });
 });
+
+// GET services page
 router.get('/services', function(req, res, next) {
   res.render('index', { title: 'Services', page: "services" });
 });
